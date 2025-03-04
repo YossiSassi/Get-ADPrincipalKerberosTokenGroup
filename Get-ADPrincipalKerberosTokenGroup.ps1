cc76d6c130e659529b698fff2cf8a06e440e7927
1nTh35h3ll <yossis@protonmail.com>
2025-03-04 21:05:51 +0200

    v1.0.5 - added a check for expired AD accounts + a new optional parameter to display all token groups, including generic default ones

--- Get-ADPrincipalKerberosTokenGroup.ps1
+++ Get-ADPrincipalKerberosTokenGroup.ps1
@@ -1,7 +1,8 @@
 Function Get-ADPrincipalKerberosTokenGroup {
 <# 
 Comments to 1nTh35h311 (@yossi_sassi)
-Version: 1.0.4
+Version: 1.0.5
+v1.0.5 - added a check for expired AD accounts + a new optional parameter to display all token groups, including generic default ones
 v1.0.4 - fixed an issue where a user wasn't locked out but the lockouttime attribute STILL had a value that prevented the script from running (moved to a WMI query instead)
 v1.0.3 - added support for other domains + minor error handling addition
 #>
@@ -10,8 +11,10 @@ param (
     [Parameter(Position=0,mandatory=$true)]
     [string]$UserName,
     [Parameter(Position=1)]
-    [string]$DomainDN = [System.String]::Empty,
+    [switch]$IncludeDefaultTokenGroups = $false,
     [Parameter(Position=2)]
+    [string]$DomainDN = [System.String]::Empty,
+    [Parameter(Position=3)]
     [string]$DomainController = [System.String]::Empty
 )
 
@@ -34,7 +37,7 @@ else
     }
 
 $ds.Filter = "(&(objectcategory=person)(objectclass=user)(samaccountname=$UserName))";
-$ds.PropertiesToLoad.AddRange(@("lockouttime","useraccountcontrol"));
+$ds.PropertiesToLoad.AddRange(@("lockouttime","useraccountcontrol","accountExpires"));
 $userObj = $ds.FindOne();
 
 # Ensure connection is successful 
@@ -50,11 +53,26 @@ $DomainName = $env:USERDNSDOMAIN;
 # enum user token
 if ($userObj)
     {
+        # Retrieve the accountExpires attribute
+        $accountExpires = [int64]$userObj.Properties.accountexpires[0]
+
+        if ($accountExpires -eq 0 -or $accountExpires -eq 9223372036854775807) {
+            # "Account never expires"
+        } else {
+            $expirationDate = [datetime]::FromFileTime($accountExpires);
+            if ($expirationDate -lt (Get-Date)) { # skip -> Account expired on: $expirationDate
+                $AccountExpired = $true
+            } 
+            # else - Account is active. Expiration date: $expirationDate
+        }
+
+        # get locked out status via WMI
         $AccountLockedWMI  = Get-WmiObject -Namespace "root\cimv2" -Class Win32_UserAccount -Filter "name='$UserName' AND domain='$DomainName'" | select -ExpandProperty LockOut;
-        if ($AccountLockedWMI -eq $True -xor $Disabled -contains $userObj.Properties.useraccountcontrol)
+        
+        if ($AccountLockedWMI -eq $True -xor $Disabled -contains $userObj.Properties.useraccountcontrol -xor $AccountExpired)
         #if ($userObj.Properties.lockouttime -ne $null -xor $Disabled -contains $userObj.Properties.useraccountcontrol)
             {
-                Write-Warning "User account is either Locked or Disabled. Can only enumerate Token Groups for enabled/active users.`nQuiting.";
+                Write-Warning "User account is either Locked, Disabled or Expired. Can only enumerate Token Groups for enabled/active users.`nQuiting.";
                 break
             }
     }
@@ -73,7 +91,7 @@ try {
     break
 }
 
-# Groups to exclude from the token
+# By default, some default generic Groups are excluded from the token, unless the -IncludeDefaultTokenGroups parameter is specified
 $ExcludedGroups = 'Everyone','BUILTIN\Users','BUILTIN\Pre-Windows 2000 Compatible Access','BUILTIN\Certificate Service DCOM Access','NT AUTHORITY\NETWORK','NT AUTHORITY\Authenticated Users','NT AUTHORITY\This Organization','Service asserted identity';
 
 # Get SIDs from Kerberos token
@@ -89,12 +107,18 @@ $groupSIDs | Foreach-Object {
                     $Group = ($_).Translate([System.Security.Principal.NTAccount]);
                     
                     # translate the SID to an account name
-                    if ($Group -notin $ExcludedGroups) {
+                    if (!$IncludeDefaultTokenGroups -and $Group -in $ExcludedGroups) { 
+                        # skip group (reserved for later use)
+                    }
+                    
+                    else 
+                        {
+                        # Add group to enumerated groups collection
                         Add-Member -InputObject $Obj -MemberType NoteProperty -Name GroupName -Value $Group.Value -Force;
                         # Get AccountType
                         if ($_.AccountDomainSid -eq $null) {$AccountType="Local"} else {$AccountType="Domain"}
                         Add-Member -InputObject $Obj -MemberType NoteProperty -Name AccountType -Value $AccountType -Force;
-                        $EnumeratedGroups += $Obj;
+                        $EnumeratedGroups += $Obj
                     }
             }
             
@@ -106,7 +130,7 @@ $groupSIDs | Foreach-Object {
 
             finally
             {
-                Clear-Variable AccountType, Obj, Group;
+                Clear-Variable AccountType, Obj, Group
             }
         }
 
