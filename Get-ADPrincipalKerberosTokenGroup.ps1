464665ec8025b872c9a31e3b76a03f8449d47b93
1nTh35h3ll <yossis@protonmail.com>
2025-03-22 14:30:47 +0200

    v1.0.8 - Added support for Computer accounts PAC Enum, including sID History + a minor display bug fix

--- Get-ADPrincipalKerberosTokenGroup.ps1
+++ Get-ADPrincipalKerberosTokenGroup.ps1
@@ -1,7 +1,8 @@
 Function Get-ADPrincipalKerberosTokenGroup {
 <# 
 Comments to 1nTh35h311 (@yossi_sassi)
-Version: 1.0.7
+Version: 1.0.8
+v1.0.8 - Added support for Computer accounts PAC Enum, including sID History. Also, a minor display bug fix not removing duplicate group when sIDHistory detected
 v1.0.7 - Fixed an issue with multiple entries in sIDHistory + moved to a simpler long-binary/hex sid conversion function + Added a new property to reflect Object Class for sIDHistory + Changed property named to better reflect the content (e.g. samaccountname rather than group name, sIDType rather than accountType)
 v1.0.6 - added sidHistory conversion and indication where sidHistory is present on the account (possible excessive permission/backdoor/etc.)
 v1.0.5 - added a check for expired AD accounts + a new optional parameter to display all token groups, including generic default ones
@@ -11,7 +12,7 @@ v1.0.3 - added support for other domains + minor error handling addition
 param (
     [cmdletbinding()]
     [Parameter(Position=0,mandatory=$true)]
-    [string]$UserName,
+    [string]$SamAccountName,
     [Parameter(Position=1)]
     [switch]$IncludeDefaultTokenGroups = $false,
     [Parameter(Position=2)]
@@ -19,10 +20,6 @@ param (
     [Parameter(Position=3)]
     [string]$DomainController = [System.String]::Empty
 )
-
-# first, check that user is Not disabled/lockedout (if True, then no use to even try to perform the check, cannot enum token)
-$Disabled = @();
-$Disabled += "514","546","66050";
 
 if ($DomainDN -ne [System.String]::Empty)
     {
@@ -38,25 +35,27 @@ else
         $ds = New-Object System.DirectoryServices.DirectorySearcher
     }
 
-$ds.Filter = "(&(objectcategory=person)(objectclass=user)(samaccountname=$UserName))";
+# Updated filter for including Computer accounts as well
+$ds.Filter = "(&(|(objectclass=user)(objectclass=computer))(samaccountname=$SamAccountName))";
+#$ds.Filter = "(&(objectcategory=person)(objectclass=user)(samaccountname=$UserName))";
 $ds.PropertiesToLoad.AddRange(@("lockouttime","useraccountcontrol","accountExpires","sidHistory"));
-$userObj = $ds.FindOne();
+$AccountObj = $ds.FindOne();
 
 # Ensure connection is successful 
 if (!$?) 
     {
-        Write-Warning "An error occured when connecting to the server. Ensure the domain & DC names are correct.`nQuiting.";
+        Write-Warning "An error occured when connecting to the server. Ensure the domain & DC names are correct.";
         break
     }
 
-# Get current user's netbios Domain Name
+# Get current netbios Domain Name
 $DomainName = $env:USERDNSDOMAIN;
 
-# enum user token
-if ($userObj)
+# enum account token
+if ($AccountObj)
     {
         # Retrieve the accountExpires attribute
-        $accountExpires = [int64]$userObj.Properties.accountexpires[0]
+        $accountExpires = [int64]$AccountObj.Properties.accountexpires[0]
 
         if ($accountExpires -eq 0 -or $accountExpires -eq 9223372036854775807) {
             # "Account never expires"
@@ -68,28 +67,35 @@ if ($userObj)
             # else - Account is active. Expiration date: $expirationDate
         }
 
-        # get locked out status via WMI
-        $AccountLockedWMI  = Get-WmiObject -Namespace "root\cimv2" -Class Win32_UserAccount -Filter "name='$UserName' AND domain='$DomainName'" | select -ExpandProperty LockOut;
-        
-        if ($AccountLockedWMI -eq $True -xor $Disabled -contains $userObj.Properties.useraccountcontrol -xor $AccountExpired)
+        # Get locked out status via WMI, for a user account only
+        if ($SamAccountName -notlike "*$") 
+            {
+                $AccountLockedWMI  = Get-WmiObject -Namespace "root\cimv2" -Class Win32_UserAccount -Filter "name='$SamAccountName' AND domain='$DomainName'" | select -ExpandProperty LockOut;
+            }
+        else
+            {
+                $AccountLockedWMI = $false
+            }
+
+        if ($AccountLockedWMI -eq $True -xor [int64]$AccountObj.Properties.useraccountcontrol[0] -band 0x0002 -xor $AccountExpired)
         #if ($userObj.Properties.lockouttime -ne $null -xor $Disabled -contains $userObj.Properties.useraccountcontrol)
             {
-                Write-Warning "User account is either Locked, Disabled or Expired. Can only enumerate Token Groups for enabled/active users.`nQuiting.";
+                Write-Warning "Account is either Locked, Disabled or Expired. Can only enumerate Token Groups for enabled/active accounts.";
                 break
             }
     }
 else
     {
-        Write-Warning "Could not find user: $UserName. Make sure you've spelled the name correctly.`nQuiting.";
+        Write-Warning "Could not find account: $SamAccountName. Make sure you've spelled the name correctly.";
         break
     }
 
-# Get user context
+# Get account security context
 try {
-    $token = [System.Security.Principal.WindowsIdentity]::new($UserName)
+    $token = [System.Security.Principal.WindowsIdentity]::new($SamAccountName)
 } catch {
     # If an error was raised, don't continue enumerating token groups
-    Write-Warning "An error occured enumerating token for user: $($UserName.ToUpper()).`nMake sure account is Not Locked out.`nException: $($Error[0].Exception.InnerException.Message)`nQuiting.";
+    Write-Warning "An error occured enumerating token for account: $($SamAccountName.ToUpper()).`nMake sure account is Not Locked out.`nException: $($Error[0].Exception.InnerException.Message)";
     break
 }
 
@@ -149,17 +155,17 @@ $groupSIDs | Foreach-Object {
             }
         }
 
-    # Handle SidHistory, IF it applicable
-    if ($userObj.Properties.sidhistory -ne $null) 
+    # Handle SidHistory, If applicable
+    if ($AccountObj.Properties.sidhistory -ne $null) 
         {
-            $sidHistoryValue =  $userObj.Properties.sidhistory | ForEach-Object {Convert-HexSID -HexSID $_ -ErrorAction SilentlyContinue}
+            $sidHistoryValue =  $AccountObj.Properties.sidhistory | ForEach-Object {Convert-HexSID -HexSID $_ -ErrorAction SilentlyContinue}
             Write-Warning "[!] SidHistory is present on this account.";
             $sidHistoryValue | ForEach-Object {
                 $sidHistoryCurrentValue = $_;
                 $sidHistoryObj = [adsi]"LDAP://<SID=$sidHistoryCurrentValue>";
                 $sidHistoryAccountName = "$env:USERDOMAIN\$($sidHistoryObj.Properties.samaccountname)";
                 # remove duplicate account from token enum array
-                $EnumeratedGroups | foreach {if ($_.GroupName -eq $sidHistoryAccountName) {$DuplicateGroupToRemove = $_}};
+                $EnumeratedGroups | foreach {if ($_.SamAccountName -eq $sidHistoryAccountName) {$DuplicateGroupToRemove = $_}};
         	    $EnumeratedGroups.Remove($DuplicateGroupToRemove) | Out-Null;
                 $Obj = New-Object psobject;
                 # Ensure we got a valid account (existing domain) for sidHistory. otherwise, display Sid
